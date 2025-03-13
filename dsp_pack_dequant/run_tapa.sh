163119eaed7ddbe490530049cf0bec379f6bb9be
Oswald He <zifanhe1202@ucla.edu>
2025-03-12 21:10:44 -0700

    file cleanup

--- dsp_pack_dequant/run_tapa.sh
+++
@@ -1,8 +0,0 @@
-tapac \
-    -o w3a6.hw.xo \
-    --platform xilinx_u280_xdma_201920_3 \
-    --top w3a6o48linear \
-    --work-dir w3a6.hw.xo.tapa \
-    --enable-synth-util \
-    --max-parallel-synth-jobs 16 \
-    w3a6o48linear_kernel.cpp 
--- fluid_sa/run_tapa.sh
+++
@@ -1,8 +0,0 @@
-tapac \
-    -o fluid.hw.xo \
-    --platform xilinx_u280_xdma_201920_3 \
-    --top fluid_spatial_kernel \
-    --work-dir fluid.hw.xo.tapa \
-    --enable-synth-util \
-    --max-parallel-synth-jobs 16 \
-    fluid_sa_kernel.cpp 
